19c1d92549d1e2846bada6b5a81d3b219d69cc39
Mark Baggett <mbagget1@utk.edu>
2019-11-26 10:18:25 -0500

    Add doc blocks for public types, procedures, and methods.

--- src/oaitools.nim
+++ src/oaitools.nim
@@ -22,6 +22,7 @@ proc write_to_disk(filename: string, contents: string, destination_directory: st
 
 type 
   OaiRequest* = ref object of RootObj
+    ## This type describes an OAI request.
     base_url*: string
     oai_set*: string
     client: HttpClient
@@ -40,6 +41,24 @@ method count_documents_on_page(this: OaiRequest, node: string): int {.base.} =
   count(node, "<header>")
 
 method get_complete_size*(this: OaiRequest, metadata_format: string): int {.base.} =
+  ## Gets the number of records in an OAI-PMH request.
+  ##
+  ## If the request passes a resumptionToken, this parses size from resumptionToken[@completeListSize].
+  ## If the request does not have a resumption token, the value is based on the number of headers in the response.
+  ##
+  ## Requires:
+  ##   metadata_format(string): The metadata format of your request.
+  ##
+  ## Returns:
+  ##   int: The total number of records in a request.
+  ##
+  ## Example:
+  ##
+  ## .. code-block:: nim
+  ##
+  ##   var x = newOaiRequest("https://dpla.lib.utk.edu/repox/OAIHandler")
+  ##   x.get_complete_size("MODS")
+  ##
   var set_string = ""
   if this.oai_set != "":
     set_string = "&set=" & this.oai_set
@@ -51,11 +70,29 @@ method get_complete_size*(this: OaiRequest, metadata_format: string): int {.base
     this.count_documents_on_page($(xml_response // "header"))
 
 method list_sets*(this: OaiRequest): seq[string] {.base.} =
+  ## Returns a sequence of sets as strings available from an OAI-PMH provider.
+  ##
+  ## Example:
+  ##
+  ## .. code-block:: nim
+  ##
+  ##   var x = newOaiRequest("https://dpla.lib.utk.edu/repox/OAIHandler")
+  ##   x.list_sets()
+  ##
   let xml_response = this.make_request(this.base_url & "?verb=ListSets")
   let results = $(xml_response // "setSpec")
   get_text_value_of_attributeless_node(results, "setSpec")
 
 method list_sets_and_descriptions*(this: OaiRequest): seq[(string, string)] {.base.} =
+  ## Returns a sequence of tuples with set name and set description available from an OAI-PMH provider.
+  ##
+  ## Example:
+  ##
+  ## .. code-block:: nim
+  ##
+  ##    var x = newOaiRequest("https://dpla.lib.utk.edu/repox/OAIHandler")
+  ##    x.list_sets_and_descriptions()
+  ##
   let xml_response = this.make_request(this.base_url & "?verb=ListSets")
   let set_specs = $(xml_response // "setSpec")
   let spec_seq = get_text_value_of_attributeless_node(set_specs, "setSpec")
@@ -67,14 +104,41 @@ method list_sets_and_descriptions*(this: OaiRequest): seq[(string, string)] {.ba
     i += 1
 
 method list_metadata_formats*(this: OaiRequest): seq[string] {.base.} =
+  ## Returns a sequence of metadata_formats available from an OAI-PMH provider.
+  ##
+  ## Example:
+  ##
+  ## .. code-block:: nim
+  ##
+  ##    var x = newOaiRequest("https://dpla.lib.utk.edu/repox/OAIHandler")
+  ##    x.list_metadata_formats()
+  ##
   let xml_response = this.make_request(this.base_url & "?verb=ListMetadataFormats")
   let prefixes = $(xml_response // "metadataPrefix")
   get_text_value_of_attributeless_node(prefixes, "metadataPrefix")
 
 method identify*(this: OaiRequest): string {.base.} =
+  ## Returns an XML file of information about an OAI-PMH provider as a string.
+  ##
+  ## Example:
+  ##
+  ## .. code-block:: nim
+  ##
+  ##    var x = newOaiRequest("https://dpla.lib.utk.edu/repox/OAIHandler")
+  ##    x.identify()
+  ##
   $(this.make_request(this.base_url & "?verb=Identify"))
 
 method list_identifiers*(this: OaiRequest, metadata_format: string): seq[string] {.base.} =
+  ## Returns a sequence of identifiers for records belonging to an OAI-PMH request.
+  ##
+  ## Example:
+  ##
+  ## .. code-block:: nim
+  ##
+  ##    var x = newOaiRequest("https://dpla.lib.utk.edu/repox/OAIHandler", "utk_wderfilms")
+  ##    x.list_identifiers("MODS")
+  ##
   var set_string = ""
   var xml_response: Node
   var token = "first_pass"
@@ -91,6 +155,20 @@ method list_identifiers*(this: OaiRequest, metadata_format: string): seq[string]
     request = this.base_url & "?verb=ListIdentifiers&resumptionToken=" & token
 
 method harvest_metadata_records*(this: OaiRequest, metadata_format: string, output_directory: string): (int, int) {.base.} =
+  ## Harvests metadata records from an OAI-PMH request to disk.
+  ##
+  ## Requires:
+  ##
+  ##   metadata_format (string): The metadata format.
+  ##   output_directory (string): The full path to where you want to write your files.
+  ##
+  ## Example:
+  ##
+  ## .. code-block:: nim
+  ##
+  ##    var x = newOaiRequest("https://dpla.lib.utk.edu/repox/OAIHandler", "utk_wderfilms")
+  ##    x.list_identifiers("MODS")
+  ##
   var set_string = ""
   var xml_response: Node
   var token = "first_pass"
@@ -111,6 +189,17 @@ method harvest_metadata_records*(this: OaiRequest, metadata_format: string, outp
   (i - 1, total_size)
 
 method list_records*(this: OaiRequest, metadata_format: string): seq[string] {.base.} =
+  ## Returns a sequence of XML records as strings for each record in a request.
+  ##
+  ## **NOTE**: Use this method with caution.  This can cause your sequence to get very big.
+  ##
+  ## Example:
+  ##
+  ## .. code-block:: nim
+  ##
+  ##    var x = newOaiRequest("https://dpla.lib.utk.edu/repox/OAIHandler", "utk_wderfilms")
+  ##    x.list_records("MODS")
+  ##
   var set_string = ""
   var xml_response: Node
   var token = "first_pass"
@@ -127,4 +216,12 @@ method list_records*(this: OaiRequest, metadata_format: string): seq[string] {.b
     request = this.base_url & "?verb=ListIdentifiers&resumptionToken=" & token
 
 proc newOaiRequest*(url: string, oai_set=""): OaiRequest =
+  ## Constructs a new Oai-PMH request.
+  ##
+  ## Example:
+  ##
+  ## .. code-block:: nim
+  ##
+  ##    var x = newOaiRequest("https://dpla.lib.utk.edu/repox/OAIHandler", "utk_wderfilms")
+  ##
   OaiRequest(base_url: url, oai_set: oai_set, client: newHttpClient())
