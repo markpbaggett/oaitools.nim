f477c906ab6c35708e6442c0edee67813e1b1e9c
Mark Baggett <mbagget1@utk.edu>
2020-02-24 12:49:44 -0500

    Add sketchy http tests with no mocks or patches. :worried:

--- tests/tests.nim
+++ tests/tests.nim
@@ -1,4 +1,4 @@
-import unittest, oaitools
+import unittest, oaitools, strutils
 
 suite "Test Initialization with Full Url":
   setup:
@@ -19,3 +19,40 @@ suite "Test Initialization with Full Url":
   
   test "oai_set is defined and accessible":
     check test_request.oai_set == "utk_derris"
+
+suite "Sketchy HTTP tests with no Patches or Mocks":
+  setup:
+    let
+      test_request = newOaiRequest("http://dpla.lib.utk.edu/repox/OAIHandler", "utk_heilman")
+      sets_and_descriptions = test_request.list_sets_and_descriptions()
+      just_sets = test_request.list_sets()
+      metadata_formats = test_request.list_metadata_formats()
+  
+  test "Check List Sets is working as expected":
+    check 200 <= len(just_sets) 
+
+  test "Check List Sets with Descriptions is working as expected":
+    check 200 <= len(sets_and_descriptions)
+  
+  test "Look for known items":
+    check ("utk_tenngirl", "Tennessee Girl Student Handbook") in sets_and_descriptions
+    check "utk_scopes" in just_sets
+
+  test "Check List Metadata Formats":
+    check "mods" in metadata_formats
+    check "MODS" in metadata_formats
+    check "oai_dc" in metadata_formats
+    check "oai_qdc" in metadata_formats
+    check "xoai" in metadata_formats
+  
+  test "Check List Identifiers and List Records":
+    let
+      records = test_request.list_records("MODS")
+      identifiers = test_request.list_identifiers("MODS")
+    
+    check 1120 == len(records)
+    check 1120 == len(identifiers)
+    check identifiers[999].startswith("urn:dpla.lib.utk.edu.utk_heilman")
+    check identifiers[42].startswith("urn:dpla.lib.utk.edu.utk_heilman")
+    check records[1100].startswith("<mods")
+    check records[300].startswith("<mods")
